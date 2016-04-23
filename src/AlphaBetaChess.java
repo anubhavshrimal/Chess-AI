c5abd0b91af133609721dc0bf3ccd964b62ebef0
Anubhav Shrimal <anubhavshrimal@gmail.com>
2016-04-23 13:00:46 +0530

    added chess board representation in array

--- src/AlphaBetaChess.java
+++ src/AlphaBetaChess.java
@@ -1,13 +1,39 @@
 import javax.swing.JFrame;
 
 public class AlphaBetaChess {
+	
+	//array for representation of the chess board
+	/*
+	 * color=WHITE/black
+	 * pawn=P/p
+	 * knight=K/k
+	 * rook=R/r
+	 * bishop=B/b
+	 * queen=Q/q
+	 * king=A/a	
+	*/
+	static String chessBoard[][]={
+	        {"r","k","b","q","a","b","k","r"},
+	        {"p","p","p","p","p","p","p","p"},
+	        {" "," "," "," "," "," "," "," "},
+	        {" "," "," "," "," "," "," "," "},
+	        {" "," "," "," "," "," "," "," "},
+	        {" "," "," "," "," "," "," "," "},
+	        {"P","P","P","P","P","P","P","P"},
+	        {"R","K","B","Q","A","B","K","R"}};
+	
 	public static void main(String[] args) {
-		JFrame f=new JFrame("My title goes here");
+		JFrame f=new JFrame("Chess");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		UserInterface ui=new UserInterface();
 		f.add(ui);
 		f.setSize(500, 500);
 		f.setVisible(true);
+	}
+	
+	//returns previous position(row1,column1), current position (row2,column2) and the captured piece if any else space
+	public static String possibleMoves(){
+		return "";
 	}
 
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import javax.swing.JPanel;
@@ -10,5 +11,48 @@ public class UserInterface extends JPanel implements MouseListener, MouseMotionL
 		super.paintComponent(g);
 		g.setColor(Color.red);
 		g.fillRect(10, 20, 100, 200);
+	
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
