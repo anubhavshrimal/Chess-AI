9e04a938f563d69be4266350044ab86d8d4892eb
Anubhav Shrimal <anubhavshrimal@gmail.com>
2016-04-23 13:14:26 +0530

    added method possibleMoves() in AlphaBetaChess.java for returning the possible move for any piece

--- src/AlphaBetaChess.java
+++ src/AlphaBetaChess.java
@@ -33,7 +33,64 @@ public class AlphaBetaChess {
 	
 	//returns previous position(row1,column1), current position (row2,column2) and the captured piece if any else space
 	public static String possibleMoves(){
-		return "";
+		String moveList="";
+		/*
+		 	checks for each square on the chess board the further possible moves of the chess pieces
+			returns the possible move for each piece and adds it to the moveList
+		*/
+		for (int i=0; i<64; i++) {
+            switch (chessBoard[i/8][i%8]) {
+                case "P": moveList+=possibleP(i);
+                    break;
+                case "R": moveList+=possibleR(i);
+                    break;
+                case "K": moveList+=possibleK(i);
+                    break;
+                case "B": moveList+=possibleB(i);
+                    break;
+                case "Q": moveList+=possibleQ(i);
+                    break;
+                case "A": moveList+=possibleA(i);
+                    break;
+            }
+        }
+		return moveList;
+	}
+	
+	public static String possibleP(int i){
+		String moveList="";
+		
+		return moveList;
+	}
+	
+	public static String possibleR(int i){
+		String moveList="";
+		
+		return moveList;
+	}
+	
+	public static String possibleK(int i){
+		String moveList="";
+		
+		return moveList;
+	}
+	
+	public static String possibleB(int i){
+		String moveList="";
+		
+		return moveList;
+	}
+	
+	public static String possibleQ(int i){
+		String moveList="";
+		
+		return moveList;
+	}
+	
+	public static String possibleA(int i){
+		String moveList="";
+		
+		return moveList;
 	}
 
 }
